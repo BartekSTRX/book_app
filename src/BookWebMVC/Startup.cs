0c322dc845c9144694f2abce94206730f5238a50
Bartłomiej Kołodziejczyk <bartek.kol93@gmail.com>
2016-02-06 12:49:16 +0100

    basic mvc setup
    - project.json dependencies,
    - HomeController, Index view, layout etc
    - Startup service and middleware configuration

--- src/BookWebMVC/Startup.cs
+++ src/BookWebMVC/Startup.cs
@@ -15,16 +15,22 @@ namespace BookWebMVC
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
             app.UseIISPlatformHandler();
+            app.UseStaticFiles();
 
-            app.Run(async (context) =>
+            app.UseMvc(conf =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                conf.MapRoute("DefaultRoute", "{controller}/{action}/{id?}", defaults: new
+                {
+                    controller = "Home",
+                    action = "Index"
+                });
             });
         }
 
