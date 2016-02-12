0e500f6ddbf894c0b8906c9c35398e031a0343c6
Bartłomiej Kołodziejczyk <bartek.kol93@gmail.com>
2016-02-12 23:58:34 +0100

    authors list
    - AuthorCtrl with Index action and view
    - ImgaeCtrl for showing image files

---
+++ src/BookWebMVC/Controllers/Web/AuthorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookWebMVC.Data.Core;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+
+
+namespace BookWebMVC.Controllers.Web
+{
+    public class AuthorController : Controller
+    {
+        private readonly BookWebContext _context;
+
+        public AuthorController(BookWebContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _context.Authors.Include(a => a.ProfilePicture).ToList();
+            return View(model);
+        }
+    }
+}
---
+++ src/BookWebMVC/Controllers/Web/ImageController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using BookWebMVC.Data.Core;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookWebMVC.Controllers.Web
+{
+    public class ImageController : Controller
+    {
+        private readonly BookWebContext _context;
+
+        public ImageController(BookWebContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Show(int id)
+        {
+            var picture = _context.Pictures.SingleOrDefault(p => p.Id == id);
+            if (picture == null)
+            {
+                return HttpNotFound();
+            }
+            try
+            {
+                var image = Image.FromFile(picture.Path);
+
+                using (var stream = new MemoryStream())
+                {
+                    image.Save(stream, ImageFormat.Jpeg);
+                    return File(stream.ToArray(), "image/jpg");
+                }
+            }
+            catch (Exception e) when(e is FileNotFoundException || e is ArgumentException)
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}
